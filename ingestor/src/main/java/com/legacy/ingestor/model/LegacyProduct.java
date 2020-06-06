7abcc4fce039d41eabd1266a908436df4353c646
Ali Jafari <ali1dc@gmail.com>
2020-06-06 10:58:35 -0400

    set application id for customer

--- ingestor/src/main/java/com/legacy/ingestor/model/LegacyProduct.java
+++ ingestor/src/main/java/com/legacy/ingestor/model/LegacyProduct.java
@@ -1,7 +1,5 @@
 package com.legacy.ingestor.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -12,37 +10,27 @@ import javax.persistence.*;
 @Getter @Setter
 @Entity
 @Table(name = "products")
-public class Product {
+public class LegacyProduct {
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @JsonProperty("id")
     @Column(name = "product_id")
-    Long id;
+    Long productId;
 
-    @JsonProperty("name")
     @Column(name = "product_name")
-    String name;
+    String productName;
 
-    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "category_id")
     Category category;
 
-    @JsonProperty("description")
     String description;
 
-    @JsonProperty("list_price")
     @Column(name = "list_price")
     Float listPrice;
 
-    @JsonProperty("quantity")
     Integer quantity;
 
-    @JsonProperty("created_by")
     @Column(name = "created_by")
     String createdBy;
-
-    @JsonProperty("legacy_id")
-    @Transient
-    Long legacyId;
 }
