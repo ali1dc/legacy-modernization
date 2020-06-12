c8e2b85b148854fe7e7d304a28a6fcf4cab56b35
Ali Jafari <ali1dc@gmail.com>
2020-06-11 21:41:13 -0400

    address table

--- payment/src/main/resources/db/migration/V1__initial.sql
+++ payment/src/main/resources/db/migration/V1__initial.sql
@@ -1,3 +1,24 @@
+CREATE TABLE public.addresses
+(
+  id INT PRIMARY KEY,
+  address_1 VARCHAR(300) NOT NULL,
+  address_2 VARCHAR(200),
+  city VARCHAR(100) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  zip VARCHAR(10) NOT NULL,
+  created_by VARCHAR(50) NOT NULL,
+  created_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_by VARCHAR(50),
+  updated_date TIMESTAMP WITHOUT TIME ZONE
+)
+WITH (
+  OIDS = FALSE
+);
+
+ALTER TABLE public.addresses OWNER to postgres;
+ALTER TABLE public.addresses REPLICA IDENTITY FULL;
+
+----------------------------------------
 CREATE TABLE public.customers
 (
   id INT PRIMARY KEY,
@@ -5,11 +26,13 @@ CREATE TABLE public.customers
   last_name VARCHAR(200) NOT NULL,
   email VARCHAR(200) NOT NULL,
   phone VARCHAR(15),
+  address_id INT,
   created_by VARCHAR(50) NOT NULL,
   created_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_by VARCHAR(50),
   updated_date TIMESTAMP WITHOUT TIME ZONE,
-  legacy_id INT
+  legacy_id INT,
+  FOREIGN KEY (address_id) REFERENCES addresses(id)
 )
 WITH (
   OIDS = FALSE
@@ -20,15 +43,13 @@ ALTER TABLE public.customers REPLICA IDENTITY FULL;
 
 ----------------------------------------
 CREATE TABLE orders(
-  id SERIAL PRIMARY KEY,
-  customer_id INT NOT NULL,
-  status VARCHAR(50) DEFAULT 'pending',
+  id INT PRIMARY KEY,
+  status VARCHAR(50) NOT NULL,
   created_by VARCHAR(50) NOT NULL,
   created_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_by VARCHAR(50),
   updated_date TIMESTAMP WITHOUT TIME ZONE,
-  legacy_id INT,
-  FOREIGN KEY (customer_id) REFERENCES customers(id)
+  legacy_id INT
 )
 WITH (
   OIDS = FALSE
@@ -43,7 +64,7 @@ CREATE TABLE payments(
   customer_id INT NOT NULL,
   order_id INT NOT NULL,
   amount NUMERIC(8, 2) NOT NULL,
-  successfull BOOLEAN DEFAULT FALSE,
+  successful BOOLEAN DEFAULT FALSE,
   created_by VARCHAR(50) NOT NULL,
   created_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_by VARCHAR(50),
