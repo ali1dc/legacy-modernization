d2e4d77e2288c753c46d38fd629cef1401e7ed63
Ali Jafari <ali1dc@gmail.com>
2020-07-10 00:53:48 -0400

    shipment simulator

--- ingestor/src/main/java/com/legacy/ingestor/repository/ShipmentRepository.java
+++ ingestor/src/main/java/com/legacy/ingestor/repository/ShipmentRepository.java
@@ -3,5 +3,9 @@ package com.legacy.ingestor.repository;
 import com.legacy.ingestor.model.LegacyShipment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ShipmentRepository extends CrudRepository<LegacyShipment, Long> {
+
+    Optional<LegacyShipment> findTopByOrderOrderId(Long orderId);
 }
