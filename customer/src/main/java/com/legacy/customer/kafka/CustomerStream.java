33c4a2d0080053fc0daa8142aa3bbfa87a1b8217
Ali Jafari <ali1dc@gmail.com>
2020-07-12 17:07:33 -0400

    refactor customer microservice for mssql legacy db

--- customer/src/main/java/com/legacy/customer/kafka/CustomerStream.java
+++ customer/src/main/java/com/legacy/customer/kafka/CustomerStream.java
@@ -1,13 +1,10 @@
 package com.legacy.customer.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.legacy.customer.dto.*;
 import com.legacy.customer.event.AddressEvent;
 import com.legacy.customer.event.CustomerAddressEvent;
 import com.legacy.customer.event.CustomerEvent;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.GlobalKTable;
 import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
@@ -26,40 +23,22 @@ public class CustomerStream {
     private ObjectMapper jsonMapper;
 
     @Bean
-    public Function<KStream<String, CustomerEvent>, KStream<String, CustomerDto>> pCustomer() {
-        return input -> input
-                .map((key, event) -> KeyValue.pair(key, event.getAfter()));
-    }
-
-    @Bean
-    public Function<KStream<String, AddressEvent>, KStream<String, AddressDto>> pAddress() {
-        return input -> input
-                .map((key, event) -> KeyValue.pair(key, event.getAfter()));
-    }
-
-    @Bean
-    public Function<KStream<String, CustomerAddressEvent>, KStream<String, CustomerAddressDto>> pCustomerAddress() {
-        return input -> input
-                .map((key, event) -> KeyValue.pair(key, event.getAfter()));
-    }
-
-    @Bean
-    public Function<KStream<String, CustomerAddressDto>,
-            Function<GlobalKTable<String, CustomerDto>,
-                    Function<GlobalKTable<String, AddressDto>,
+    public Function<KStream<String, CustomerAddressEvent>,
+            Function<GlobalKTable<String, CustomerEvent>,
+                    Function<GlobalKTable<String, AddressEvent>,
                             KStream<String, EnrichedCustomer>>>> enrichedCustomers() {
         return customerAddresses -> (
                 customers -> (
                         addresses -> (
                                 customerAddresses.leftJoin(customers,
-                                        (customerAddressId, customerAddress) -> customerAddress.getCustomerId().toString(),
+                                        (customerAddressId, customerAddress) -> customerAddress.getAfter().getCustomerId().toString(),
                                         CustomerAddressCustomer::new)
                                         .leftJoin(addresses,
-                                                (id, customerAddressCustomer) -> customerAddressCustomer.getCustomerAddress().getAddressId().toString(),
+                                                (id, customerAddressCustomer) -> customerAddressCustomer.getCustomerAddressEvent().getAfter().getAddressId().toString(),
                                                 (customerAddressCustomer, address) -> EnrichedCustomer.builder()
-                                                        .customer(customerAddressCustomer.getCustomer())
-                                                        .customerAddress(customerAddressCustomer.getCustomerAddress())
-                                                        .address(address)
+                                                        .customer(customerAddressCustomer.getCustomerEvent().getAfter())
+                                                        .customerAddress(customerAddressCustomer.getCustomerAddressEvent().getAfter())
+                                                        .address(address.getAfter())
                                                         .build())
                         )
                 )
